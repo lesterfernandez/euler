566f59a2d60814628ff5b1bccfab155d9e0cb668
lesterfernandez <5958256+lesterfernandez@users.noreply.github.com>
2022-10-05 13:51:26 -0400

    first commit 🚀

---
+++ 3_largest_prime_factor.py
@@ -0,0 +1,14 @@
+def getLargestFactor(n):
+    divisor = 2
+    while n % 2 == 0:
+      n /= 2
+
+    divisor = 3
+    while n > 1:
+      if n % divisor == 0:
+          n /= divisor
+      else: divisor += 2
+    return divisor
+
+n = 600851475143
+print(getLargestFactor(n))
---
+++ 4_largest_palindrome_product.py
@@ -0,0 +1,26 @@
+def isPalindrome(s):
+    i, j = 0, len(s) - 1
+    while i < j:
+        if s[i] != s[j]: return False
+        i += 1
+        j -= 1
+    return True
+
+def getLargestPalindrome(n):
+    lowerBound = pow(10, n - 1)
+    upperBound = pow(10, n)
+    factors = (1, 1)
+    palindrome = 1
+    for i in range(lowerBound, upperBound):
+        for j in range(i, upperBound):
+            product = i * j
+            if isPalindrome(str(product)) and product > palindrome:
+                palindrome = product
+                factors = i, j
+    return [palindrome, factors]
+
+
+n = input("Number of digits for largest palindrome: ")
+while not n.isdigit():
+    n = input("Please enter an integer: ")
+print(getLargestPalindrome(int(n)))
