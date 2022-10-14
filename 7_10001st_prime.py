3d5721094e7d903b4657f91897abb0dfc3b3f7ec
lesterfernandez <5958256+lesterfernandez@users.noreply.github.com>
2022-10-13 20:24:54 -0400

    refactor: generalized problem 7

--- 7_10001st_prime.py
+++ 7_10001st_prime.py
@@ -1,20 +1,15 @@
 # Time complexity: O(n^2)
 # Space complexity: O(n)
+def getNthPrime(n: int) -> int:
+    primes = [2]
+    current = 3
+    while len(primes) < n:
+        for prime in primes:
+            if current % prime == 0:
+                break
+        else: # runs if we did not break the loop
+            primes.append(current)
+        current += 2 
+    return primes[-1]
 
-primes = [2]
-current = 3
-while len(primes) < 10001:
-    #  Using list comprehension:
-    #  primeNotFactors = [n for n in primes if current % n != 0]
-    #  if len(primeNotFactors) == len(primes): # none of the primes were a factor
-    #      primes.append(current)
-    #  current += 2
-
-    for n in primes:
-        if current % n == 0:
-            break
-    else: # if we did not break the loop
-        primes.append(current)
-    current += 2
-
-print(f"THE ANSWER IS: {primes[-1]}")
+print(f"THE ANSWER IS: {getNthPrime(10001)}")
